6d36e743b1b26eec7515857e6c3ee0e4b0c73b5a
carsonwong0712 <cwong@pfprobotics.com>
2020-04-24 03:45:54 +1000

    added post update product request

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,26 +4,50 @@ exports.getAddProduct = (req, res, next) => {
     res.render("admin/edit-product", {
         pageTitle: "Add Product",
         path: "/admin/add-product",
+        editing: false
     });
 };
 
 exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, description, price } = req.body;
-    const product = new Product(title, imageUrl, description, price);
+    //new product
+    const product = new Product(null, title, imageUrl, description, price);
     product.save();
     res.redirect("/");
 };
 
+//edit action
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit; // /:productId?edit=true
     if (!editMode) {
         return res.redirect("/");
     }
-    res.render("admin/edit-product", {
-        pageTitle: "Add Product",
-        path: "/admin/edit-product",
-        editing: editMode
+    const { productId } = req.params;
+    Product.findProdById(productId, product => {
+        if (!product) {
+            return res.redirect("/");
+        }
+        res.render("admin/edit-product", {
+            pageTitle: "Edit Product",
+            path: "/admin/edit-product",
+            editing: editMode,
+            product: product
+        });
     });
+};
+
+//update product action
+exports.postEditProduct = (req, res, next) => {
+    //fetch product info
+    //input "name"
+    const { productId } = req.body;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+    const updatedProduct = new Product(productId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice);
+    updatedProduct.save();
+    res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
