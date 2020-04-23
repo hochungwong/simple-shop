3c621359f09cced70f7612ac26941f683c468a40
carsonwong0712 <cwong@pfprobotics.com>
2020-04-24 02:54:50 +1000

    added getEditProduct controllers and routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,13 +1,9 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    console.log("In another middleware!");
-    res.render("admin/add-product", {
+    res.render("admin/edit-product", {
         pageTitle: "Add Product",
         path: "/admin/add-product",
-        activeAddProduct: true,
-        formsCSS: true,
-        productCSS: true,
     });
 };
 
@@ -16,6 +12,18 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit; // /:productId?edit=true
+    if (!editMode) {
+        return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+        pageTitle: "Add Product",
+        path: "/admin/edit-product",
+        editing: editMode
+    });
 };
 
 exports.getProducts = (req, res, next) => {
