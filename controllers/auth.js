6ec8ade802a597e4c5f75c95a72a7c0c483bd9a3
carsonwong0712 <cwong@pfprobotics.com>
2020-06-13 15:25:42 +1000

    implemented login and signup functions (hashed pwd)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
@@ -11,14 +12,66 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5ed73183498b953fe7b2b769")
+  const { email, password } = req.body;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
-      });
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            //save user to session
+            req.session.user = user;
+            //save session to database then redirect to index page
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/login");
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Sign Up",
+    isAuthenticated: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const { email, password, confirmPassword } = req.body;
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        //existing user
+        return res.redirect("/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/login");
+        });
     })
     .catch((err) => {
       console.log(err);
--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,10 @@ router.get("/login", authController.getLogin);
 
 router.post("/login", authController.postLogin); //mimic login atm
 
+router.get("/signup", authController.getSignup);
+
+router.post("/signup", authController.postSignup);
+
 router.post("/logout", authController.postLogout);
 
 module.exports = router;
